43703146d5d7837b706d936f7ee7ec3879252cd6
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-11-19 12:04:47 +0100

    UD3.ACT1-3.intento1.inicial

---
+++ UD3/ACT1-3/Actividad1.java
@@ -0,0 +1,32 @@
+//Problem solving
+// Only appears 1 address. Put the getAllByName and no the getByName
+package ejerciciosClase;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * The Class Actividad1.
+ */
+public class Actividad1 {
+
+	/**
+	 * Method that runs the application.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println("Direcciones asociadas a Spotify:");
+		try {
+			InetAddress[] ad=InetAddress.getAllByName("www.spotify.com");
+			for(InetAddress a:ad) {
+				System.out.println(a);
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
