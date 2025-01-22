e480437fe7fb97527c6c70c8b99d5b206980b3fd
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2025-01-22 10:22:30 +0100

    UD4.ACT5.intento1.definitivo

---
+++ UD4/ACT5/ObtieneFichero.java
@@ -0,0 +1,17 @@
+package ejercicio5_UD4;
+
+import java.io.Serializable;
+
+public class ObtieneFichero implements Serializable{
+
+	//Declaracion atributo con el contenido en bytes del fichero a descargar
+	byte[] contenidoFichero;
+	
+	//constructor
+	public ObtieneFichero(byte[] contenidoFichero) {
+		this.contenidoFichero=contenidoFichero;
+	}
+	
+	//Metodo para obetner el fichero a descargar
+	public byte[] getContenidoFichero() {return contenidoFichero;}
+}
